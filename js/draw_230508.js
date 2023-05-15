326de99313dba5972587ba174dbcac5e0f453595
psm <1828421@donga.ac.kr>
2023-05-15 16:42:01 +0900

    class data commit

--- js/draw_230508.js
+++ js/draw_230508.js
@@ -1,6 +1,6 @@
 var scene = new THREE.Scene();
 var renderer = new THREE.WebGLRenderer({ antialias: true });
-var camera = new THREE.PerspectiveCamera(75, 1.0, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera( 75 , 1.0, 0.1, 1000);
 
 function initLight() {
     var pointLight = new THREE.PointLight(0xffffff);
@@ -10,8 +10,8 @@ function initLight() {
 }
 
 function initRenderer() {
-    camera.position.z = 10;
-    renderer.setClearColor("#000000");
+    camera.position.z = 15;
+    renderer.setClearColor("#FFFFFF");
     renderer.setSize(500, 500);
     // Append Renderer to DOM
     document.body.appendChild(renderer.domElement);
@@ -20,15 +20,24 @@ function initGeometry() {
     // const axesHelper = new THREE.AxesHelper(); //x:red y:green z:blue 
     // scene.add(axesHelper);
 
-    var material0 = new THREE.MeshLambertMaterial({ color: "#ff0000" });
+    var material0 = new THREE.MeshLambertMaterial({ color: "#8B00FF" });
     var geometryCube = new THREE.BoxGeometry(1, 1, 1);
+    var geometrySphere = new THREE.SphereGeometry(1);
+    var geometryCone = new THREE.ConeGeometry(1);
 
-    for (var i = 0; i < 5; i++) {
-        for (var j = 0; j < 5; j++) {
+
+    //6번 지멋대로 큐브 속도 돌게 해바라
+
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            var material0 = new THREE.MeshLambertMaterial({ color: "#8B00FF" });
+            material0.color.setHex(Math.random()* 0xffffff);
+            if(i%2==0)
+            material0.wireframe=true
             var cube = new THREE.Mesh(geometryCube, material0);
             //Translation
-            cube.translateX(-4.0 + 2.0 * i);
-            cube.translateY(-4.0 + 2.0 * j);
+            cube.translateX(-9.0 + 2.0 * i);
+            cube.translateY(-9.0 + 2.0 * j);
             // Add cube to Scene
             scene.add(cube);
         }
@@ -45,7 +54,12 @@ function init() {
 var render = function () {
     requestAnimationFrame(render);
     for (var i = 1; i < scene.children.length; i++) {
-        scene.children[i].rotation.x += 0.01;
+        scene.children[i].rotation.x += Math.random()*0.01;
+        if(i/scene.children.length<Math.random()){
+            if(i%3==0) scene.children[i].rotation.y += Math.random()*0.01;
+            else if (i%3==1) scene.children[i].rotation.z += Math.random()*0.01;
+            else scene.children[i].rotation.x += Math.random()*0.002;
+        }
     }
     renderer.render(scene, camera);
 
---
+++ js/draw_230515.js
@@ -0,0 +1,75 @@
+var scene = new THREE.Scene();
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+var camera = new THREE.PerspectiveCamera(75, 1.0, 0.1, 1000);
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+var index = 0;
+
+controls.enableDamping = false; // 부드러운 감속 효과 활성화
+
+function loadOBJ(url) {
+  var loader = new THREE.OBJLoader();
+  // instantiate a loader
+  // load a resource
+  loader.load(
+    // resource URL
+    url,
+    // called when resource is loaded
+    function (object) {
+
+      scene.add(object);
+
+    },
+    // called when loading is in progresses
+    function (xhr) {
+
+      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+    },
+    // called when loading has errors
+    function (error) {
+
+      console.log('An error happened');
+
+    }
+  );
+}
+
+function initLight() {
+  var pointLight0 = new THREE.PointLight(0xffffff);
+  pointLight0.position.set(10, 0, 10);
+  scene.add(pointLight0);
+}
+
+function initGeometry() {
+  const axesHelper = new THREE.AxesHelper(); //x:red y:green z:blue 
+  scene.add(axesHelper);
+  loadOBJ("../models/kitten.obj");
+
+}
+
+function initRenderer() {
+  camera.position.z = 1;
+  controls.update();
+  renderer.setClearColor("#ffffff");
+  renderer.setSize(500, 500);
+  // Append Renderer to DOM
+  document.body.appendChild(renderer.domElement);
+}
+
+function init() {
+  initLight();
+  initGeometry();
+  initRenderer();
+}
+
+// Render Loop
+var render = function () {
+  requestAnimationFrame(render);
+  index++;
+  controls.update();
+  renderer.render(scene, camera);
+
+};
+
+init();
+render();
